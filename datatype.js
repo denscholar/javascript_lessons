ed3e76b1da9c362b3f9261a7a58a0fde1b987f57
Dennis Akagha <d.akagha20@gmail.com>
2024-10-18 00:13:09 +0100

    datatypes lessons

---
+++ datatype.js
@@ -0,0 +1,48 @@
+// Data types
+/*
+1. Number: Infinity, NaN
+2. BigInt 
+3. String
+4. Boolean: The boolean type has only two values: true and false.
+5. null
+6. undefined
+7. Objects and Symbols
+8. typeof
+
+
+
+*/
+
+// using a the back tic in string. It allows you to extend functionalities
+
+let name = "John";
+
+console.log(`${name}!`);
+
+let num1 = 50
+let num2 = 100
+// let name = 'Dennis'
+
+console.log(`${num1 + num2}`);
+console.log(`Hello ${name}`);
+
+let number;
+console.log(number);
+
+console.log("------------");
+number = 200;
+
+console.log(number);
+
+number = 'Dennis'
+
+console.log(typeof(number));
+
+alert("Hello world")
+
+
+
+
+
+
+
---
+++ type_conversiion.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+// String Conversion
+let value = true;
+console.log(value);
+
+value = String(value);
+
+console.log(value);
+
+console.log(typeof(value));
+
+// numerc conversion
+
+let number_1 = 50;
+let newNumber = String(number_1);
+
+console.log(newNumber);
+
+console.log(typeof(newNumber));
+
+let numeric = Number(newNumber)
+
+console.log(typeof(numeric));
+
+
+
+
+
+
